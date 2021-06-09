70e0a7e8f56825e2ae002c7f0245e6b5187ff42a
Brendan Armani <brendanarmani@gmail.com>
2021-06-09 09:19:23 -0400

    initial commit

---
+++ src/main/java/com/api/RecipeApp/RecipeAppApplication.java
@@ -0,0 +1,14 @@
+package com.api.RecipeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeAppApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Revving up!");
+		SpringApplication.run(RecipeAppApplication.class, args);
+	}
+
+}
